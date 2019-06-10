e2ec7c0d38a70333ca3c820f8ef6b2ffbf74c5fa
Filipe Amaral <filipe.santos.serra.amaral@gmail.com>
2019-06-10 18:19:14 +0100

    mails, password, confirm email
    sendgrid

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -2,6 +2,8 @@
 const jwt = require("jsonwebtoken");
 const Roles = require("_helpers/roles");
 const User = require("models/user");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(config.sendGridApi);
 
 // users hardcoded for simplicity, store in a db for production applications
 //  const users = [
@@ -10,22 +12,60 @@ const User = require("models/user");
 //  ];
 
 module.exports = {
+  registration,
+  emailconfirmed,
   authenticate,
   getAll,
-  getById
+  getById,
+  passwordReset,
+  passwordForgotten
 };
+
+async function registration(user) {
+  let _user = new User(user);
+  _user.password = _user.generateHash(user.password);
+  let newUser = await _user.save();
+  if (!newUser) return;
+  newUser = JSON.parse(JSON.stringify(newUser));
+  const token = jwt.sign({ domain: config.domain, email: newUser.email }, config.secret);
+  const msg = {
+    to: newUser.email,
+    from: config.emailFrom,
+    subject: "AdotAqui Email",
+    html: config.emailTemplate.replace(new RegExp("#LINKTOKEN#", "g"), config.domain + "/users/token/" + token)
+  };
+  sgMail.send(msg);
+
+  return {
+    success: true
+  };
+}
+
+async function emailconfirmed(token) {
+  const _email = jwt.verify(token, config.secret, function(err, decoded) {
+    if (err) return;
+    return decoded.email;
+  });
+
+  if (!_email) return;
+
+  const _user = await User.findOneAndUpdate({ email: _email }, { emailconfirmed: true });
+  return {
+    success: true,
+    ..._user.toDictionary()
+  };
+}
 
 async function authenticate({ email, password }) {
   const user = await User.findOne({ email: email })
     .populate("role")
     .exec();
-  if (!user) return;
-  if (!user.validPassword(password)) return;
-  const token = jwt.sign(
-    { sub: user.id, role: user.role.title },
-    config.secret
-  );
+  if (!user) return { success: false, message: "Email or password is incorrect" };
+  if (!user.emailconfirmed) return { success: false, message: "Email is not active yet." };
+  if (!user.validPassword(password)) return { success: false, message: "Email or password is incorrect" };
+  const token = jwt.sign({ sub: user.id, role: user.role.title }, config.secret);
   return {
+    success: true,
     ...user.toDictionary(),
     role: user.role.title,
     token
@@ -37,7 +77,7 @@ async function getAll() {
     .populate("role")
     .exec();
 
-  var a = users.map(u => {
+  users.map(u => {
     return { ...u.toDictionary(), role: u.role.title };
   });
   return users.map(u => {
@@ -51,4 +91,40 @@ async function getById(id) {
     .exec();
   if (!user) return;
   return { ...user.toDictionary(), role: user.role.title };
+}
+
+async function passwordReset(sub = null, password, token = null) {
+  if (token !== null) {
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return;
+      sub = decoded.number;
+    });
+  }
+
+  let _user = await User.findById(sub);
+
+  if (!_user) return;
+  _user.generateHash(password);
+  const user = await User.findByIdAndUpdate(sub, { password: _user.password });
+  return {
+    ...user.toDictionary()
+  };
+}
+
+async function passwordForgotten(email) {
+  const _user = await User.findOne({ email: email }).exec();
+  if (!_user) return;
+  const token = jwt.sign({ domain: config.domain, email: _user.email, number: _user.id }, config.secret);
+  const link = config.domain + "/users/password/reset/" + token;
+  const msg = {
+    to: email,
+    from: config.emailFrom,
+    subject: "AdotAqui Password",
+    html: '<h3>Definir nova password:</h3><p><a href="' + link + '">' + link + "</a></p>"
+  };
+  sgMail.send(msg);
+
+  return {
+    success: true
+  };
 }
