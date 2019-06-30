d0d1c7459ccebc27f0169af96b52d20032d39176
César Nero <cesar.augusto.nero@hotmail.com>
2019-06-30 05:51:32 +0100

    Update specie.js

--- api/models/specie.js
+++ api/models/specie.js
@@ -4,7 +4,8 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var specieSchema = mongoose.Schema({
     name        : String,
     name_PT     : String,
-    breeds      : [{ type: ObjectId, ref: 'Breed' }]
+    breeds      : [{ type: ObjectId, ref: 'Breed' }],
+    icon        : String
 });
 
 module.exports = mongoose.model('Specie', specieSchema);
